4a2e38db608d9b4d9314fb4db4e6d29f1f476bdf
salimovrashad <selimovresad33@gmail.com>
2024-07-08 02:43:44 +0400

    add firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDsK342g5L5I3AXKBEczoeZFksk2Kq67FQ",
-  authDomain: "motormania-az.firebaseapp.com",
-  databaseURL: "https://motormania-az-default-rtdb.firebaseio.com",
-  projectId: "motormania-az",
-  storageBucket: "motormania-az.appspot.com",
-  messagingSenderId: "1072264523538",
-  appId: "1:1072264523538:web:459110a81f145ddcdb0dcb"
+  apiKey: "AIzaSyCVkihoyA8fFftDQ1SYoxk75mXXOgVQFyA",
+  authDomain: "motormania-b0b5b.firebaseapp.com",
+  databaseURL: "https://motormania-b0b5b-default-rtdb.firebaseio.com",
+  projectId: "motormania-b0b5b",
+  storageBucket: "motormania-b0b5b.appspot.com",
+  messagingSenderId: "453564061781",
+  appId: "1:453564061781:web:6e27c5bd7d84f3837a1cdc"
 };
 
 // Initialize Firebase
